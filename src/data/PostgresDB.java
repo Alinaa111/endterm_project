10a5e7dece7c6a3251033eeee04d51f4bc4dfb25
alina <alinamoldabaeva1704@gmail.com>
2025-02-07 02:17:33 +0500

    Implemented PostgresDB class with connection

--- src/data/PostgresDB.java
+++ src/data/PostgresDB.java
@@ -1,4 +1,28 @@
 package data;
 
-public class PostgresDB {
+import data.interfaces.IDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresDB implements IDB {
+    private static PostgresDB instance;
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/food_order";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "aitu@111";
+
+    private PostgresDB() {}
+
+    public static PostgresDB getInstance() {
+        if (instance == null) {
+            instance = new PostgresDB();
+        }
+        return instance;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
