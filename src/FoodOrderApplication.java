11906810f2755b0498d7a49248498a2f7b688fef
alina <alinamoldabaeva1704@gmail.com>
2025-02-10 22:06:32 +0500

    Implemented FoodOrderApplication and added element to PostgresDB

--- src/FoodOrderApplication.java
+++ src/FoodOrderApplication.java
@@ -1,2 +1,41 @@
+import controllers.RecipeController;
+
+import java.util.Scanner;
+
 public class FoodOrderApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        RecipeController recipeController = new RecipeController();
+
+        System.out.println("Welcome to the Food Order Application!");
+
+        while (true) {
+            System.out.println("\nChoose a category:");
+            System.out.println("1. Entree (Soups)");
+            System.out.println("2. Second Dishes");
+            System.out.println("3. Drinks");
+            System.out.println("4. Desserts");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice: ");
+            int categoryChoice = scanner.nextInt();
+
+            if (categoryChoice == 5) {
+                System.out.println("Thank you for using the Food Order Application. Goodbye!");
+                break;
+            }
+
+            recipeController.displayRecipesByCategory(categoryChoice);
+
+            System.out.print("\nChoose a recipe (Enter ID): ");
+            int recipeChoice = scanner.nextInt();
+
+            System.out.print("For how many portions? ");
+            int portions = scanner.nextInt();
+
+            recipeController.displayRecipeDetails(recipeChoice, portions);
+        }
+
+        scanner.close();
+    }
 }
